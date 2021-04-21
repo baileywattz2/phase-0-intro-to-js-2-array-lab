4c2d49a9f23bd43a2975b9c2a3415ec2fa384096
bailey watkins <bailey.watkins@rocketmail.com>
2021-04-20 20:27:39 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+
+let cats=["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+   cats.unshift(name)
+
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+    const allCats=[...cats,"Broom"]
+    return allCats
+}
+
+function prependCat(name){
+    const allCats=[name , ...cats]
+    return allCats
+}
+
+function removeLastCat(){
+   const allCats = cats.slice(0,2)
+   return allCats
+}
+
+function removeFirstCat(){
+    const allCats = cats.slice(1)
+    return allCats
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -44,7 +44,7 @@ describe('index.js', function () {
       });
     });
 
-    describe('appendCat(name)', function () {
+    describe(' ', function () {
       it('appends a cat to the cats array and returns a new array, leaving the cats array unchanged', function () {
         expect(appendCat("Broom")).to.have.ordered.members(["Milo", "Otis", "Garfield", "Broom"]);
 
